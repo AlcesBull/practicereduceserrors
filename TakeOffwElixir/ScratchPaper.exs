4041e8fb6fd9f6a1f92a8635b7beda25dcfe0871
AlcesBull <spencer@alcesbull.com>
2017-10-25 08:14:59 -0400

    Changed my scratchpaper to .exs for scripting format

---
+++ TakeOffwElixir/ScratchPaper.exs
@@ -0,0 +1,13 @@
+#Scratch Paper
+
+defmodule Scratch do
+  def convert_to_light_seconds({:miles, miles}) when
+  is_integer(miles) do
+    miles * 5.36819e-6
+  end
+
+  def convert_to_light_seconds({:meters, meters}) when
+  is_integer(meters) do
+    meters * 3.335638620368e-9
+  end
+end
